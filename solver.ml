13d34f68cdd65e8285c0bd5c64af943b33aa609f
daiki <daiki.matsuoka@outlook.jp>
2021-08-07 20:55:27 +0900

    update: debug mode

--- solver.ml
+++ solver.ml
@@ -2,13 +2,17 @@ open Syntax
 
 exception SolveErr
 
+let is_debug_mode =
+	try (Sys.argv.(1) = "-d") with
+	| Invalid_argument _ -> false
+
 (*
-原子論理式(あるいはその否定)のリストを受け取り、その中に矛盾があるかどうかを判定し、以下を返す
-- 矛盾があれば空リスト
-- 矛盾がなければ、原子論理式への付値
+原子論理式(あるいはその否定)のリストを受け取り、以下を返す
+- そのリストが矛盾していれば空リスト
+- そうでなければ、そのリストに整合する原子論理式への付値
  *)
-let is_closed branch =
-	let rec is_closed_inner branch acc =
+let find_valuation branch =
+	let rec find_valuation_inner branch acc =
 		match branch with
 		| [] -> acc
 		| fml :: rest ->
@@ -18,46 +22,116 @@ let is_closed branch =
 					[] (* 矛盾したら終わり *)
 				else
 					if List.mem fml acc then
-						is_closed_inner rest acc (* 既に入っている場合は加えない *)
+						find_valuation_inner rest acc (* 既に入っている場合は加えない *)
 					else
-						is_closed_inner rest (fml :: acc)
+						find_valuation_inner rest (fml :: acc)
 			| FNot (FVar x) ->
 				if List.mem (FVar x) acc then
 					[]
 				else
 					if List.mem fml acc then
-						is_closed_inner rest acc
+						find_valuation_inner rest acc
 					else
-						is_closed_inner rest (fml :: acc)
+						find_valuation_inner rest (fml :: acc)
 			| _ -> raise SolveErr
-	in is_closed_inner branch []
+	in find_valuation_inner branch []
 
 (*
-	fml_list: 探索の際に使う論理式の集合, 
-	branch: 探索の中で出てきた原子論理式(あるいはその否定)のリスト(tableauが分岐する場合はこれも分岐する)
+- fml_list: 探索の際に使う論理式の集合, 
+- branch: 探索の中で出てきた原子論理式(あるいはその否定)のリスト(tableauが分岐する場合はこれも分岐する)
 *)
+let level = ref 0
+let after_fork = ref false
 let rec search_tableau fml_list branch = 
 	match fml_list with
-	| [] -> [is_closed branch]
+	| [] ->
+		let v = find_valuation branch in
+			(if is_debug_mode then
+				if v = [] then
+					print_endline_in_tree "→ contradiction" !level
+				else
+					print_endline_in_tree ("→ valuation found: " ^ string_of_valuation v) !level
+			else ());
+			[v]
 	| fml :: rest ->
+		(if is_debug_mode then
+			if !after_fork then
+				(after_fork := false; print_endline (string_of_formula fml (* ^ ", " ^ string_of_formula_list rest *)))
+			else
+				print_endline (string_of_formula_in_tree fml !level (* ^ ", " ^ string_of_formula_list rest*))
+		else ());
 		match fml with
 		| FVar _ -> search_tableau rest (fml :: branch) (* 原子論理式の場合はbranchに追加 *)
-		| FIf (fml1, fml2) -> search_tableau (FNot fml1 :: rest) branch @ search_tableau (fml2 :: rest) branch
+		| FIf (fml1, fml2) ->
+			(if is_debug_mode then
+				(print_string_in_tree "- " !level;
+				level := !level + 2;
+				after_fork := true)
+			else ());
+			let v1 = search_tableau (FNot fml1 :: rest) branch in
+			(if is_debug_mode then
+				(level := !level - 2;
+				print_string_in_tree "- " !level;
+				level := !level + 2;
+				after_fork := true)
+			else ());
+			let v2 = search_tableau (fml2 :: rest) branch in
+				(if is_debug_mode then level := !level - 2 else ());
+				v1 @ v2
 		| FAnd (fml1, fml2) -> search_tableau (fml1 :: fml2 :: rest) branch
-		| FOr (fml1, fml2) -> search_tableau (fml1 :: rest) branch @ search_tableau (fml2 :: rest) branch
+		| FOr (fml1, fml2) ->
+			(if is_debug_mode then
+				(print_string_in_tree "- " !level;
+				level := !level + 2;
+				after_fork := true)
+			else ());
+			let v1 = search_tableau (fml1 :: rest) branch in
+			(if is_debug_mode then
+				(level := !level - 2;
+				print_string_in_tree "- " !level;
+				level := !level + 2;
+				after_fork := true)
+			else ());
+			let v2 = search_tableau (fml2 :: rest) branch in
+				(if is_debug_mode then level := !level - 2 else ());
+				v1 @ v2
 		| FNot fml' ->
 			match fml' with
 			| FVar _ -> search_tableau rest (fml :: branch) (* 原子論理式の否定の場合はbranchに追加 *)
 			| FIf (fml1, fml2) -> search_tableau (fml1 :: FNot fml2 :: rest) branch
-			| FAnd (fml1, fml2) -> search_tableau (FNot fml1 :: rest) branch @ search_tableau (FNot fml2 :: rest) branch
+			| FAnd (fml1, fml2) ->
+				(if is_debug_mode then
+					(print_string_in_tree "- " !level;
+					level := !level + 2;
+					after_fork := true)
+				else ());
+				let v1 = search_tableau (FNot fml1 :: rest) branch in
+				(if is_debug_mode then
+					(level := !level - 2;
+					print_string_in_tree "- " !level;
+					level := !level + 2;
+					after_fork := true)
+				else ());
+				let v2 = search_tableau (FNot fml2 :: rest) branch in
+					(if is_debug_mode then level := !level - 2 else ());
+					v1 @ v2
 			| FOr (fml1, fml2) -> search_tableau (FNot fml1 :: FNot fml2 :: rest) branch
 			| FNot fml'' -> search_tableau (fml'' :: rest) branch
 
+(* 推論を受け取り、反例モデルを返す(妥当な場合は空リスト) *)
 let solve inf = 
 	match inf with
 	| Inf (cn, pr) ->
 		let init_list = FNot pr :: cn in
+		(if is_debug_mode then
+			(print_endline "\n===== search start =====";
+			level := 0;
+			after_fork := false)
+		else ());
 		let counter_models = search_tableau init_list [] in
+		(if is_debug_mode then
+			print_endline "===== search end =====\n"
+		else ());
 		let remove_dup list =
 			let rec remove_dup_inner list acc =
 				match list with
--- syntax.ml
+++ syntax.ml
@@ -5,7 +5,7 @@ type formula =
 	| FOr of formula * formula
 	| FIf of formula * formula
 
-type inference = Inf of formula list * formula (* premisses and conclusion *)
+type inference = Inf of formula list * formula (* premises and conclusion *)
 
 let rec string_of_formula fml =
 	match fml with
@@ -18,26 +18,37 @@ let rec string_of_formula fml =
 let string_of_formula_list fmls =
 	let rec string_of_formula_list_inner fmls flag =
 		match fmls with
-		| [] -> ""
+		| [] -> "]"
 		| fml :: rest ->
 			if flag then
 				string_of_formula fml ^ string_of_formula_list_inner rest false
 			else
 				", " ^ string_of_formula fml ^ string_of_formula_list_inner rest false
-	in string_of_formula_list_inner fmls true
+	in "[" ^ string_of_formula_list_inner fmls true
 
 let string_of_inference inf =
 	match inf with
-	| Inf (pr, cn) -> string_of_formula_list pr ^ " |- " ^ string_of_formula cn
+	| Inf (pr, cn) -> "primeses: " ^ string_of_formula_list pr ^ ", " ^ "conclusion: " ^ string_of_formula cn
 
 exception StringifyErr
 let string_of_valuation v =
 	let rec string_of_valuation_inner v flag =
 		match v with
-		| [] -> ""
+		| [] -> "}"
 		| fml :: rest ->
 			match fml with
-			| FVar x -> if flag then x ^ "->true" ^ string_of_valuation_inner rest false else ", " ^ x ^ "->true" ^ string_of_valuation_inner rest false
-			| FNot (FVar x) -> if flag then x ^ "->false" ^ string_of_valuation_inner rest false else ", " ^ x ^ "->false" ^ string_of_valuation_inner rest false
+			| FVar x -> if flag then x ^ ":true" ^ string_of_valuation_inner rest false else ", " ^ x ^ ":true" ^ string_of_valuation_inner rest false
+			| FNot (FVar x) -> if flag then x ^ ":false" ^ string_of_valuation_inner rest false else ", " ^ x ^ ":false" ^ string_of_valuation_inner rest false
 			| _ -> raise StringifyErr
-	in string_of_valuation_inner v true
+	in "{" ^ string_of_valuation_inner v true
+
+
+(* tree-display (for debug) *)
+let rec n_space n =
+	if n = 0 then "" else " " ^ n_space (n-1)
+
+let print_string_in_tree s level = print_string (n_space level ^ s)
+
+let print_endline_in_tree s level = print_endline (n_space level ^ s)
+
+let string_of_formula_in_tree fml level = n_space level ^ string_of_formula fml
